c200ed6651e4329d311fbbca2f700d09867b4727
Gustavo Henrique de Oliveira <87510192+guta231@users.noreply.github.com>
2024-11-21 16:25:53 -0300

    Update sketch_nov19a.ino

--- sketch_arduino/sketch_nov19a.ino
+++ sketch_arduino/sketch_nov19a.ino
@@ -2,17 +2,17 @@
 
 
 void setup() {
-  // Inicia a comunicação serial
+ 
   Serial.begin(9600);
   pinMode(A0, INPUT);
 }
 
 void loop() {
-  // Solicita aos sensores para obter a leitura da temperatura
+  
 
   int luminosidade = analogRead(A0);
   Serial.println(luminosidade);
   
-  // Espera 1 segundo antes de ler novamente
+ 
   delay(1000);
 }
